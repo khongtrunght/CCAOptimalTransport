29b555b64b0f5ef9931abd3f509aa393e1ccbb78
khongtrunght <khongtrunght@gmail.com>
2022-06-24 15:05:29 +0700

    add print 2 view

--- src/ccaOT.py
+++ src/ccaOT.py
@@ -205,7 +205,11 @@ def main(args):
         if iter % args.draw_tsne_step == 0:
             # wandb.log(
             #     {"project_matrix_view1":  ccaot.linear_cca.w[0], "mean_view1": ccaot.linear_cca.m[0]}, step=iter)
-            tsne_plot(val1_project, label_val1, 2)
+            projected_data = np.concatenate(
+                [val1_project, val2_project], axis=0)
+            projected_labels = np.concatenate([label_val1, label_val2], axis=0)
+            tsne_plot(projected_data, projected_labels)
+            # tsne_plot(val1_project, label_val1, 2)
             wandb.log({'tsne_val1': wandb.Image(plt)}, step=iter)
             plt.clf()
             if abs(f_norm_previous - f_norm) < 1e-2:
